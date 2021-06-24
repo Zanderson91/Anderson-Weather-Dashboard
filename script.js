dfa20ef72e4a7d984789d5dd275109c978b4d4ff
Zack Anderson <zack.anderson2011@gmail.com>
2021-06-24 09:03:27 -0500

    Adding variables and IDs to elements

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const APIkey = "7743391ae0557cf69b59c68a0e3c7325"
 
 let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+let search = $("#search");
+let seacrchBtn = $("#search-btn");
+let selectCity = $("#select-city");
+let currentTemp = $("#")
+
 let city ="";
-let 
+
+
 
 
 fetch(queryURL)
