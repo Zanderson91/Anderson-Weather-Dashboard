e672b36e4397b399066e84c4e028dfcf942c73a9
Zack Anderson <zack.anderson2011@gmail.com>
2021-06-24 08:53:36 -0500

    Added simple CSS styling for bodyuu

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const APIkey = "7743391ae0557cf69b59c68a0e3c7325"
 
 let queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
-let city;
+let city ="";
+let 
 
 
 fetch(queryURL)
